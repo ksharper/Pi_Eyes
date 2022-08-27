7636d46609ba8f31bad7d2679d7693d262bbd9b7
Kyle Harper <kylesharper@gmail.com>
2022-08-26 21:20:05 -0500

    tracking tweaks

--- motion.py
+++ motion.py
@@ -35,15 +35,15 @@ while True:
     thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
   
     # Finding contour of moving object
-     cnts,_ = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts,_ = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
   
-     for contour in cnts:
-         if cv2.contourArea(contour) < 10000:
-             continue
+    for contour in cnts:
+        if cv2.contourArea(contour) < 10000:
+            continue
 
-         (x, y, w, h) = cv2.boundingRect(contour)
-         # making green rectangle around the moving object
-         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        (x, y, w, h) = cv2.boundingRect(contour)
+        # making green rectangle around the moving object
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
  
     # Displaying image in gray_scale
 #    cv2.imshow("Gray Frame", gray)
