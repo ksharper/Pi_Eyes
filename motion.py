fb0f16ad6c356761d50a79c7a89a45c1e27725c0
Kyle Harper <kylesharper@gmail.com>
2022-08-26 19:40:34 -0500

    tracking tweaks

--- motion.py
+++ motion.py
@@ -1,108 +1,114 @@
-# Author: Addison Sears-Collins
-# Description: This algorithm detects objects in a video stream
-#   using the Gaussian Mixture Model background subtraction method. 
- 
-# import the necessary packages
-from picamera.array import PiRGBArray # Generates a 3D RGB array
-import time # Provides time-related functions
-import cv2 # OpenCV library
-import numpy as np # Import NumPy library
- 
- 
-# Generates a 3D RGB array and stores it in rawCapture
-raw_capture = cv2.VideoCapture(0)
- 
-# Create the background subtractor object
-# Feel free to modify the history as you see fit.
-back_sub = cv2.createBackgroundSubtractorMOG2(history=150,
-  varThreshold=25, detectShadows=True)
- 
-# Wait a certain number of seconds to allow the camera time to warmup
-time.sleep(0.1)
- 
-# Create kernel for morphological operation. You can tweak
-# the dimensions of the kernel.
-# e.g. instead of 20, 20, you can try 30, 30
-kernel = np.ones((20,20),np.uint8)
- 
-# Capture frames continuously from the camera
-for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
-     
-    # Grab the raw NumPy array representing the image
-    image = frame.array
- 
-    # Convert to foreground mask
-    fg_mask = back_sub.apply(image)
-     
-    # Close gaps using closing
-    fg_mask = cv2.morphologyEx(fg_mask,cv2.MORPH_CLOSE,kernel)
-       
-    # Remove salt and pepper noise with a median filter
-    fg_mask = cv2.medianBlur(fg_mask,5)
-       
-    # If a pixel is less than ##, it is considered black (background). 
-    # Otherwise, it is white (foreground). 255 is upper limit.
-    # Modify the number after fg_mask as you see fit.
-    _, fg_mask = cv2.threshold(fg_mask, 127, 255, cv2.THRESH_BINARY)
- 
-    # Find the contours of the object inside the binary image
-    contours, hierarchy = cv2.findContours(fg_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[-2:]
-    areas = [cv2.contourArea(c) for c in contours]
+
+# Python program to implement 
+# Webcam Motion Detector
   
-    # If there are no countours
-    if len(areas) < 1:
+# importing OpenCV, time and Pandas library
+import cv2, time, pandas
+# importing datetime class from datetime library
+from datetime import datetime
   
-      # Display the resulting frame
-      cv2.imshow('Frame',image)
+# Assigning our static_back to None
+static_back = None
   
-      # Wait for keyPress for 1 millisecond
-      key = cv2.waitKey(1) & 0xFF
+# List when any moving object appear
+motion_list = [ None, None ]
   
-      # Clear the stream in preparation for the next frame
-      raw_capture.truncate(0)
-     
-      # If "q" is pressed on the keyboard, 
-      # exit this loop
-      if key == ord("q"):
+# Time of movement
+time = []
+  
+# Initializing DataFrame, one column is start 
+# time and other column is end time
+df = pandas.DataFrame(columns = ["Start", "End"])
+  
+# Capturing video
+video = cv2.VideoCapture(0)
+  
+# Infinite while loop to treat stack of image as video
+while True:
+    # Reading frame(image) from video
+    check, frame = video.read()
+  
+    # Initializing motion = 0(no motion)
+    motion = 0
+  
+    # Converting color image to gray_scale image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  
+    # Converting gray scale image to GaussianBlur 
+    # so that change can be find easily
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+  
+    # In first iteration we assign the value 
+    # of static_back to our first frame
+    if static_back is None:
+        static_back = gray
+        continue
+  
+    # Difference between static background 
+    # and current frame(which is GaussianBlur)
+    diff_frame = cv2.absdiff(static_back, gray)
+  
+    # If change in between static background and
+    # current frame is greater than 30 it will show white color(255)
+    thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
+  
+    # Finding contour of moving object
+    cnts,_ = cv2.findContours(thresh_frame.copy(), 
+                       cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  
+    for contour in cnts:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        motion = 1
+  
+        (x, y, w, h) = cv2.boundingRect(contour)
+        # making green rectangle around the moving object
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+  
+    # Appending status of motion
+    motion_list.append(motion)
+  
+    motion_list = motion_list[-2:]
+  
+    # Appending Start time of motion
+    if motion_list[-1] == 1 and motion_list[-2] == 0:
+        time.append(datetime.now())
+  
+    # Appending End time of motion
+    if motion_list[-1] == 0 and motion_list[-2] == 1:
+        time.append(datetime.now())
+  
+    # Displaying image in gray_scale
+    cv2.imshow("Gray Frame", gray)
+  
+    # Displaying the difference in currentframe to
+    # the staticframe(very first_frame)
+    cv2.imshow("Difference Frame", diff_frame)
+  
+    # Displaying the black and white image in which if
+    # intensity difference greater than 30 it will appear white
+    cv2.imshow("Threshold Frame", thresh_frame)
+  
+    # Displaying color frame with contour of motion of object
+    cv2.imshow("Color Frame", frame)
+  
+    key = cv2.waitKey(1)
+    # if q entered whole process will stop
+    if key == ord('q'):
+        # if something is movingthen it append the end time of movement
+        if motion == 1:
+            time.append(datetime.now())
         break
-     
-      # Go to the top of the for loop
-      continue
   
-    else:
-         
-      # Find the largest moving object in the image
-      max_index = np.argmax(areas)
-       
-    # Draw the bounding box
-    cnt = contours[max_index]
-    x,y,w,h = cv2.boundingRect(cnt)
-    cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),3)
+# Appending time of motion in DataFrame
+for i in range(0, len(time), 2):
+    df = df.append({"Start":time[i], "End":time[i + 1]}, ignore_index = True)
   
-    # Draw circle in the center of the bounding box
-    x2 = x + int(w/2)
-    y2 = y + int(h/2)
-    cv2.circle(image,(x2,y2),4,(0,255,0),-1)
+# Creating a CSV file in which time of movements will be saved
+df.to_csv("Time_of_movements.csv")
   
-    # Print the centroid coordinates (we'll use the center of the
-    # bounding box) on the image
-    text = "x: " + str(x2) + ", y: " + str(y2)
-    cv2.putText(image, text, (x2 - 10, y2 - 10),
-      cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-          
-    # Display the resulting frame
-    cv2.imshow("Frame",image)
-     
-    # Wait for keyPress for 1 millisecond
-    key = cv2.waitKey(1) & 0xFF
+video.release()
   
-    # Clear the stream in preparation for the next frame
-    raw_capture.truncate(0)
-     
-    # If "q" is pressed on the keyboard, 
-    # exit this loop
-    if key == ord("q"):
-      break
- 
-# Close down windows
+# Destroying all the windows
 cv2.destroyAllWindows()
